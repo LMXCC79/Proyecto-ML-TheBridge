4ece80fa228d67cf78e5b0afa1b6c6c3513adbcc
LMXCC79 <145386613+LMXCC79@users.noreply.github.com>
2024-02-22 09:21:19 +0100

    cambios

--- src/utils/funciones.py
+++ src/utils/funciones.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 def data_report(df):
     '''Esta funcion describe los campos de un dataframe de pandas de forma bastante clara, crack'''
@@ -36,3 +37,31 @@ def verificador_unidades(strings):
             else:
                 unidades.add(unit)
     return unidades
+
+# Aplicar One_Hot_Encoder a Train y a Test
+def apply_onehot_encoder(train:pd.DataFrame, columns_to_encode:list, test:pd.DataFrame=None):
+    
+    # Resetear índices para evitar desalineación
+    train = train.reset_index(drop=True)
+    
+    # Crear el OneHotEncoder
+    encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+
+    # Ajustar y transformar las columnas seleccionadas
+    transformed_data = encoder.fit_transform(train[columns_to_encode])
+
+    # Crear un DataFrame con las columnas transformadas
+    transformed_df = pd.DataFrame(transformed_data, columns=encoder.get_feature_names_out(columns_to_encode))
+    
+    # Concatenar con el DataFrame original excluyendo las columnas transformadas
+    df_concatenated = pd.concat([train.drop(columns_to_encode, axis=1), transformed_df], axis=1)
+
+    # Si se proporciona un segundo DataFrame, aplicar la misma transformación
+    if test is not None:
+        test = test.reset_index(drop=True)
+        transformed_data_to_transform = encoder.transform(test[columns_to_encode])
+        transformed_df_to_transform = pd.DataFrame(transformed_data_to_transform, columns=encoder.get_feature_names_out(columns_to_encode))
+        df_to_transform_concatenated = pd.concat([test.drop(columns_to_encode, axis=1), transformed_df_to_transform], axis=1)
+        return df_concatenated, df_to_transform_concatenated
+
+    return df_concatenated
